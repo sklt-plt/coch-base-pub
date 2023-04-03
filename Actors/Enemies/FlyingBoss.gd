e39f53126a50bb48d2876abac05df8b0127c6394
JM <qbamix@gmail.com>
2023-04-03 17:50:17 +0200

    FlyingBoss: WIP Retreat for bombs state

--- Actors/Enemies/FlyingBoss.gd
+++ Actors/Enemies/FlyingBoss.gd
@@ -7,18 +7,23 @@ enum States {
 	Rising_With_Snipers,
 	Following_With_Snipers,
 	Stagger,
-	Retreating_For_Bombs
+	Retreating_For_Bombs,
+	Rising_With_Bombs,
+	Bombing
 }
 
-const STARTING_HEALTH = 50.0
 var health = STARTING_HEALTH
-
 var current_state = States.Idle
+var retreat_xz
 
-const MOVEMENT_SPEED = 10
+const STARTING_HEALTH = 50.0
+const MOVEMENT_SPEED = 20
+const RETREATING_SPEED = 35
 const RISING_SPEED = 17
 const FLOAT_HEIGHT = 22.5
 const DISTANCE_TO_KEEP = 30
+const DISTANCE_TO_HIDE = 8
+const HIDE_OFFSET = 100
 const LOS_OFFSET = Vector3(0, 2.65, 0)
 
 func _physics_process(delta):
@@ -49,36 +54,54 @@ func _physics_process(delta):
 		States.Following_With_Snipers:
 			var player_node = $"/root/Player"
 
-			var my_xz_translation = Vector3(self.global_translation.x, FLOAT_HEIGHT, self.global_translation.z)
-			var player_xz_translation = Vector3(player_node.global_translation.x, FLOAT_HEIGHT, player_node.global_translation.z)
+			var my_xz_translation = find_own_xz_translation()
+			var player_xz_translation = find_player_xz_translation()
 			var target_xz = player_xz_translation
 
 			var space_state = get_world().direct_space_state
 			var result = space_state.intersect_ray(self.global_translation, player_node.global_translation + LOS_OFFSET, [self])
 
 			# can we see player?
+			# if so let's find spot we can see them from
 			if !result or (result and result.collider != player_node):
 				target_xz = find_visible_spot_above_player(space_state, player_node)
 				if my_xz_translation.distance_to(target_xz) > 1.0:
-					self.translate((target_xz - my_xz_translation).normalized() * MOVEMENT_SPEED * delta)
+					move_to_target(target_xz, my_xz_translation, delta, MOVEMENT_SPEED)
+					#self.translate((target_xz - my_xz_translation).normalized() * MOVEMENT_SPEED * delta)
 
+			# otherwise just chase
 			elif my_xz_translation.distance_to(target_xz) > DISTANCE_TO_KEEP:
-				self.translate((target_xz - my_xz_translation).normalized() * MOVEMENT_SPEED * delta)
-
+				move_to_target(target_xz, my_xz_translation, delta, MOVEMENT_SPEED)
+				
 			if self.health < STARTING_HEALTH * 0.7:
 				begin_state(States.Stagger)
 				$"%SniperBoner/AI".health = 0
 				$"%SniperBoner2/AI".health = 0
-
-func find_visible_spot_above_player(var space_state, var player_node):
-	var offset = Vector3(DISTANCE_TO_KEEP, FLOAT_HEIGHT, 0)
-	for rot in range(0, 120):
-		var test = player_node.global_translation + offset.rotated(Vector3.UP, deg2rad(rot * 3))
-		var test_result = space_state.intersect_ray(test, player_node.global_translation + LOS_OFFSET, [self])
-		if test_result and test_result.collider == player_node:
-			return Vector3(test.x, FLOAT_HEIGHT, test.z)
-
-	return Vector3(player_node.global_translation.x, FLOAT_HEIGHT, player_node.global_translation.z)
+				
+		States.Retreating_For_Bombs:
+			var my_xz_translation = find_own_xz_translation()
+			
+			if my_xz_translation.distance_to(retreat_xz) > DISTANCE_TO_HIDE:
+				move_to_target(retreat_xz, my_xz_translation, delta, RETREATING_SPEED)
+				
+			elif not is_zero_approx(retreat_xz.y):
+				retreat_xz.y = 0.0
+					
+			else:
+				begin_state(States.Rising_With_Bombs)
+				
+		States.Rising_With_Bombs:
+			if self.global_translation.y < $"/root/Player".global_translation.y + FLOAT_HEIGHT:
+				self.translate(Vector3(0, RISING_SPEED, 0) * delta)
+			else:
+				begin_state(States.Bombing)
+				  
+		States.Bombing:
+			var my_xz_translation = find_own_xz_translation()
+			var target_xz = find_player_xz_translation()
+			
+			if my_xz_translation.distance_to(target_xz) > DISTANCE_TO_KEEP:
+				move_to_target(target_xz, my_xz_translation, delta, MOVEMENT_SPEED)
 
 func begin_state(var state):
 	match state:
@@ -92,9 +115,47 @@ func begin_state(var state):
 			$AnimationPlayer.play("Stagger")
 
 		States.Retreating_For_Bombs:
-			print("aa")
+			set_retreat_xz()
 
 	current_state = state
+	print("Current State: ",state)
+	
+func set_retreat_xz():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+			
+	var offset = Vector3(0, FLOAT_HEIGHT, 0)
+	if randi()%1 != 0:
+		offset.x = -HIDE_OFFSET
+	else: 
+		offset.x = HIDE_OFFSET
+				
+	if randi()%1 != 0:
+		offset.z = -HIDE_OFFSET
+	else:
+		offset.z = HIDE_OFFSET
+	
+	retreat_xz = self.global_translation + offset
+
+func find_own_xz_translation():
+	return Vector3(self.global_translation.x, FLOAT_HEIGHT, self.global_translation.z)
+	
+func find_player_xz_translation():
+	return Vector3($"/root/Player".global_translation.x, FLOAT_HEIGHT, 
+		$"/root/Player".global_translation.z)
+
+func move_to_target(var target_xz, var my_xz_translation, var delta, var speed):
+	self.translate((target_xz - my_xz_translation).normalized() * speed * delta)
+
+func find_visible_spot_above_player(var space_state, var player_node):
+	var offset = Vector3(DISTANCE_TO_KEEP, FLOAT_HEIGHT, 0)
+	for rot in range(0, 120):
+		var test = player_node.global_translation + offset.rotated(Vector3.UP, deg2rad(rot * 3))
+		var test_result = space_state.intersect_ray(test, player_node.global_translation + LOS_OFFSET, [self])
+		if test_result and test_result.collider == player_node:
+			return Vector3(test.x, FLOAT_HEIGHT, test.z)
+
+	return Vector3(player_node.global_translation.x, FLOAT_HEIGHT, player_node.global_translation.z)
 
 func _on_BossBat_tree_exiting():
 	$"/root/Player/HUD".deregister_boss_health()
