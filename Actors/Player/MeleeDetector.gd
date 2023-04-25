cc2d969203fbb640b119f62b0e53ac0fed309779
Jakub <qbamix@gmail.com>
2023-04-25 22:28:57 +0200

    MeleeDetector: handle attacking with invincibility interval

--- Actors/Player/MeleeDetector.gd
+++ Actors/Player/MeleeDetector.gd
@@ -1,11 +1,22 @@
 extends Area
 
+const PUSH_FORCE_MUL = 3.75
+
 var attackers = []
+var iframe_timer : Timer
+
+func _ready():
+	iframe_timer = $IFrameTimer
 
 func _physics_process(_delta):
+	if not iframe_timer.is_stopped():
+		return
+
 	for a in attackers:
 		if a.is_attacking():
-			get_parent().deal_damage(a.get_direct_damage(), a.get_direct_damage(), a.global_transform.origin, a)
+			get_parent().deal_damage(a.get_direct_damage(), a.get_direct_damage()*PUSH_FORCE_MUL, a.global_transform.origin, a)
+			iframe_timer.start()
+			return
 
 func _on_MeleeDetector_area_entered(area):
 	if area is MeleeArea and not attackers.has(area):
