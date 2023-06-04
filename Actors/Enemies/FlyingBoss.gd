60ab9ce82f4b162f2023ae38228920762517dc35
Jakub <qbamix@gmail.com>
2023-06-04 13:49:30 +0200

    Change boss name

--- Actors/Enemies/FlyingBoss.gd
+++ Actors/Enemies/FlyingBoss.gd
@@ -134,7 +134,7 @@ func _physics_process(delta):
 func begin_state(var state):
 	match state:
 		States.Begin:
-			$"/root/Player/HUD".register_boss_health(self, "Flap Mouse")
+			$"/root/Player/HUD".register_boss_health(self, "Batrick")
 			$"/root/Player".give("s_kills_possible", 1)
 			$"%SniperBoner".set_awake(true)
 			set_retreat_xz()
